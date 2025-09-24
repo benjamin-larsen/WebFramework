c1014a5ac0c2991972e3d7a1f5c659101eda3f90
Benjamin <33324686+benjamin-larsen@users.noreply.github.com>
2025-09-24 13:54:16 +0200

    Add plugin arguments

--- framework/plugins.js
+++ framework/plugins.js
@@ -5,9 +5,9 @@ export const plugins = new Set();
 
 const globalPluginContext = Object.freeze({ enableRenderTiming });
 
-export function usePlugin(plugin) {
+export function usePlugin(plugin, ...args) {
   if (getCurrentInstance()) throw Error("Can't use plugin within Component.");
   if (plugins.has(plugin)) throw Error('Plugin already exists');
   plugins.add(plugin);
-  plugin(globalPluginContext);
+  plugin(globalPluginContext, ...args);
 }
